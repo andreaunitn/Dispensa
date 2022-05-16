b226e26a6cbfdbbf6d79625c1f0b672cdca98941
andreaunitn <andrea.tomasoni-1@studenti.unitn.it>
2022-05-16 16:20:08 +0200

    creata interfaccia home

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path')
+
 app.get('/', (req, res) => {
-    res.send("Hello, world!")
+    res.sendFile(path.join(__dirname + '/home.html'))
 })
 
 app.listen(port, () => {
