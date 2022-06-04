c598b7ac58c5d68b5e232d30ad6bcc5097fabcd5
Andrea <105369215+andreaunitn@users.noreply.github.com>
2022-06-04 16:20:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const db_url = process.env.DB_URL//  process.env.DB_URL;
 
-async mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
+await mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
 .then (() => {
 
     console.log("Database connection successful");
